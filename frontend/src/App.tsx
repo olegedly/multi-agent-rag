4d2e9640e409acedd4e21a43980e35b393062094
olegedly <olegmorket@gmail.com>
2026-06-05 17:25:03 +0300

    test another front-end change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,13 @@ import type { Component } from "solid-js";
 
 const App: Component = () => {
   return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello Tailwind!</p>
+    <div>
+      <h1>Ugly front-end change</h1>
+      <p class="text-4xl text-green-700 text-center py-20">Hello Tailwind!</p>
+      <p class="text-4xl text-yellow-700 text-center py-20 mt-10">
+        Test to see if a front-end change takes hold.
+      </p>
+    </div>
   );
 };
 
