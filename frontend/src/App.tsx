3b9f625a271dd076691ff7b15eea944d31a7ebe0
olegedly <olegmorket@gmail.com>
2026-06-05 17:29:34 +0300

    back to clean front-end slate. CI/CD works.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,7 @@ import type { Component } from "solid-js";
 
 const App: Component = () => {
   return (
-    <div>
-      <h1>Ugly front-end change</h1>
-      <p class="text-4xl text-green-700 text-center py-20">Hello Tailwind!</p>
-      <p class="text-4xl text-yellow-700 text-center py-20 mt-10">
-        Test to see if a front-end change takes hold.
-      </p>
-    </div>
+    <p class="text-4xl text-green-700 text-center py-20">Hello Tailwind!</p>
   );
 };
 
